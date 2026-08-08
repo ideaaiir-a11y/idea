56b45052d121d5e9bef5bd9e8c2ab491403e0fb5
ideaaiir-a11y <ideaaiir@gmail.com>
2026-08-08 08:43:23 +0330

    idea

--- .open-next/cloudflare/init.js
+++ .open-next/cloudflare/init.js
@@ -47,7 +47,7 @@ function initRuntime() {
   };
   Object.assign(globalThis, {
     Request: CustomRequest,
-    __BUILD_TIMESTAMP_MS__: 1786164761417,
+    __BUILD_TIMESTAMP_MS__: 1786165452294,
     __NEXT_BASE_PATH__: "",
     __ASSETS_RUN_WORKER_FIRST__: false,
     __TRAILING_SLASH__: false,
